061621307323714fa6ee1721c28712dd056a40de
Cyrus Correll <146882925+CyrusCorrell@users.noreply.github.com>
2023-10-03 14:15:10 -0700

    Add files via upload

---
+++ CyrusCorrellCJ11.py
@@ -0,0 +1,7 @@
+def printoutput():
+	print("Hi! My name is Cyrus Correll\nMy Pronouns are He/Him \nMy favorite movie is Extinction on Netflix \nMy favorite foods are California burritos and cabbage (separately of course)")
+def main():
+	printoutput()
+
+if __name__ == '__main__':
+	main()
